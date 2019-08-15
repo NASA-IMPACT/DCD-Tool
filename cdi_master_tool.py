91424ce9ac7c7fb58232d4e495c0b9f0fa959213
Patrick Staton <pstaton@vt.edu>
2019-08-15 09:38:38 -0500

    Update with output files.

--- cdi_master_tool.py
+++ cdi_master_tool.py
@@ -18,10 +18,10 @@ import urllib
 import csv
 
 # Retrieve "CDI Master List" from GitHub
-raw_json = urllib.request.urlopen("https://raw.githubusercontent.com/fkcook/cdi_master/master/cdi_master.json")
+github_json = urllib.request.urlopen("https://raw.githubusercontent.com/fkcook/cdi_master/master/cdi_master.json")
 
 # Format json contents
-json_reader = json.load(raw_json)
+json_reader = json.load(github_json)
 
 # Format CSV output
 fields = [['API URL', 'Title', 'Name', 'Catalog URL', 'CDI Theme']]
@@ -29,53 +29,52 @@ fields = [['API URL', 'Title', 'Name', 'Catalog URL', 'CDI Theme']]
 # Initialize containers
 broken_urls = []
 working_urls = []
-dropped_urls = []
+dropped_from_cdi = []
 values_container = []
 api_url_list = []
 
 # Loop through and remove links and "None" values
 print('[*] Starting Code [*]')
-with open('CDI_CSV_Output.csv', 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerows(fields)
-    for json_entry in json_reader:
-        api_url_list.append(json_entry)
-        if json_entry['api_url']:
-           api_url = json_entry['api_url']
-           if requests.get(api_url).status_code != 200:
-              print('Found a broken link...')
-              broken_urls.append(api_url)
-           else:
-              prepped_api = urllib.request.urlopen(api_url)
-              opened_api = json.load(prepped_api)
-              if opened_api['result']['groups']:
-                  groups_list = opened_api['result']['groups']
-                  for list_entry in groups_list:
-                      values_container.append(list_entry['name'])
-                  if 'climate5434' not in values_container:
-                      print(opened_api['result']['name'])
-                      dropped_urls.append(opened_api['result']['name'])
-                      csvwriter = [[api_url, json_entry['title'], json_entry['name'], json_entry['cdi_themes']]]
-                      del values_container[:]
-                  else:
-                      del values_container[:]
+for first_json in json_reader:
+    api_url_list.append(first_json)
+    if first_json['api_url']:
+       first_api = first_json['api_url']
+       if requests.get(first_api).status_code != 200:
+          print('Found a broken link...')
+          broken_urls.append(first_api)
+       else:
+          working_urls.append(first_api)
+          second_api_opener = urllib.request.urlopen(first_api)
+          second_api = json.load(second_api_opener)
+          if second_api['result']['groups']:
+              groups_list = second_api['result']['groups']
+              for list_entry in groups_list:
+                  values_container.append(list_entry['name'])
+              if 'climate5434' not in values_container:
+                  dropped_from_cdi.append(second_api['result']['name'])
+                  del values_container[:]
               else:
-                  print(opened_api['result']['name'])
-                  dropped_urls.append(opened_api['result']['name'])
-        else:
-            broken_urls.append(json_entry)
+                  del values_container[:]
+          else:
+              dropped_from_cdi.append(second_api['result']['name'])
+    else:
+        broken_urls.append(first_api)
 
 # Create text files for Broken URLs and Dropped URLs
-print('\n[*] Generating "broken_urls.txt" and "dropped_urls.txt" [*]')
-with open('broken_urls.txt', 'w') as broken_textfile:
+print('\n[*] Generating text files [*]')
+with open('working_urls.txt', 'w') as outfile:
+    for entry in working_urls:
+        outfile.write(entry + '\n')
+with open('broken_urls.txt', 'w') as outfile:
     for entry in broken_urls:
-        broken_textfile.write(entry + '\n')
-with open('dropped_urls.txt', 'w') as dropped_textfile:
-    for entry in dropped_urls:
-        dropped_textfile.write(entry + '\n')
+        outfile.write(entry + '\n')
+with open('dropped_from_cdi.txt', 'w') as outfile:
+    for entry in dropped_from_cdi:
+        outfile.write(entry + '\n')
 
 print("<!> COMPLETE <!>")
 print("\n == List lengths == \n")
 print('Total number of APIs pinged: ', len(api_url_list))
+print('Total number of working URLs: ', len(working_urls))
 print('Total number of broken URLs: ', len(broken_urls))
-print('Total number of dropped URLs: ', len(dropped_urls))
+print('Total number of dropped URLs: ', len(dropped_from_cdi))
